308cfaee1495b35a9ef785c6a1797592c958f120
gdoteof <gdoteof@gmail.com>
2023-05-08 01:10:40 -0400

    stuff

--- packages/failcat_home/src/index.ts
+++ packages/failcat_home/src/index.ts
@@ -1,30 +1,36 @@
-import json2html from "node-json2html";
-const { render, component } = json2html;
-
 export interface Env {
-  failcat_db: D1Database;
+	failcat_db: D1Database;
 }
 
-let template_table_body = {
-  "<>": "tr", "html": [
-      {"<>": "td", "html": "${Performance}"}
-  ]
-}
+import { scrapeVin } from "./vinlookup";
 
 export default {
-  async fetch(request: Request, env: Env) {
-    const { pathname } = new URL(request.url);
+async fetch(request: Request, env: Env) {
+		const { pathname, searchParams } = new URL(request.url);
+    const offset = searchParams.get("offset") || 0;
+    const perPage = searchParams.get("perPage") || 10;
 
-    if (pathname === "/api/cars") {
-      const { results } = await env.failcat_db.prepare(
-        "SELECT * FROM cars order by id desc limit 10"
-      )
-      .all();
-      return render(results, {}, {});
+		// Now with query params for pagination
+		if (pathname === "/api/cars") {
+			try {
+				const { results } = await env.failcat_db
+					.prepare("SELECT * FROM cars order by id desc limit ? offset ?")
+          .bind(offset, perPage)
+					.all();
+				const response = Response.json(results);
+				response.headers.set("Access-Control-Allow-Origin", "*");
+				return response;
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			} catch (e: any) {
+				console.log({ message: e.message, error: e.error });
+				return new Response(e.message, { status: 500 });
+			}
+		} else if (pathname === "/api/vin") {
+      const [car, dealer, carModel] = await scrapeVin('5XYP34GC5PG389249');
+      console.log("VIN lookup", car, dealer, carModel);
+      return "lol";
     }
 
-    return new Response(
-      "Call /api/cars to see a few cars"
-    );
-  },
+		return new Response("Call /api/cars to see a few cars");
+	},
 };
---
+++ packages/failcat_home/src/models.ts
@@ -0,0 +1,28 @@
+export class Car {
+	constructor(
+		public id: number,
+		public vin: string,
+		public ext_color: string,
+		public int_color: string,
+		public car_model: string,
+		public opt_code: string,
+		public ship_to: string,
+		public sold_to: string,
+		public created_date: Date,
+		public serial_number: number,
+		public model_year: number
+	) {}
+}
+
+export class Dealer {
+	constructor(
+		public dealer_code: string,
+		public address: string,
+		public zip: string,
+		public car_count: number
+	) {}
+}
+
+export class CarModel {
+	constructor(public model_code: string, public description: string) {}
+}
